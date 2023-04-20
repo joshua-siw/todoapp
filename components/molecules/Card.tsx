54826d1759453f0548591d1c33f9be75e84783eb
JoshuaSiwek <joshua.siwek@jaemacom.de>
2023-04-20 22:10:16 +0200

    pass props over navigation to card

--- components/molecules/Card.tsx
+++ components/molecules/Card.tsx
@@ -1,19 +1,28 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as React from "react";
 import { useState } from "react";
-import { Avatar, Card as ListCard, TextInput } from "react-native-paper";
+import {
+  Avatar,
+  Card as ListCard,
+  TextInput,
+  Checkbox,
+} from "react-native-paper";
 import storeData from "../../functions/allFunctions";
 import Button from "../atoms/Button";
 import Text from "../atoms/Text";
 import DatePicker from "./DatePicker";
+import { addTodoEntry } from "../../functions/db-service";
 
 interface CardProps {
   title: string;
 }
 
-const saveTodoElement = (title: string) => {
+const saveTodoElement = (title: string, completed, date, id?) => {
   console.log("storing data");
-  storeData("hello", "hi");
+  if (id) {
+  } else {
+    addTodoEntry("13", true, "task", db);
+  }
 };
 
 interface todoObject {
@@ -23,24 +32,36 @@ interface todoObject {
 
 const LeftContent = (props) => <Avatar.Icon icon="star" {...props} />;
 
-const Card = ({ title }) => {
-  const [date, setDate] = useState(new Date());
+const Card = ({ route, navigation }) => {
+  const { title, id, completed, date } = route.params;
+
+  const [dates, setDate] = useState(new Date());
+
+  const [checked, setChecked] = useState(completed);
+
+  const [titles, setTitle] = useState(title);
 
   return (
     <ListCard>
-      <ListCard.Title title={title} />
+      <ListCard.Title title={titles} />
       <ListCard.Content>
-        <TextInput label="Todo title" />
-        <DatePicker date={date} setDate={setDate} />
+        <TextInput label={titles} />
+        <DatePicker date={dates} setDate={setDate} />
         <Button
           title={"save"}
-          onPress={() => saveTodoElement(title)}
+          onPress={() => saveTodoElement(title, completed, date)}
           icon={undefined}
         ></Button>
         <Button
           onPress={() => console.log(date)}
           title={undefined}
           icon={"bug"}
+        />
+        <Checkbox
+          status={checked ? "checked" : "unchecked"}
+          onPress={() => {
+            setChecked(!checked);
+          }}
         />
       </ListCard.Content>
     </ListCard>
