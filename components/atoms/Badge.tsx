7d7d53d3fc43f7c50405a34e4359f4dcb92ebf27
Joshua Siwek <joshua.siwek@jaemacom.de>
2023-04-25 10:38:24 +0200

    show badge if leftdays > 0

--- components/atoms/Badge.tsx
+++ components/atoms/Badge.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { View } from "react-native";
 import { Badge as InfoBadge } from "react-native-paper";
 
 interface BadgeProps {
@@ -11,16 +12,20 @@ const daysLeft = (date) => {
   const currentDate = new Date();
   const difference = dateObj.getTime() - currentDate.getTime();
   const remainingDays = Math.ceil(difference / 86400000);
-  return remainingDays.toString();
+  return remainingDays;
 };
 
 const badgeValue = ({ date }) => {
   const remainingDays = daysLeft(date);
-  return remainingDays;
+  return remainingDays.toString();
 };
 const Badge = ({ date }) => {
   // const days = daysLeft(date);
-  return <InfoBadge>{daysLeft(date)}</InfoBadge>;
+  return (
+    <View>
+      {daysLeft(date) > 0 && <InfoBadge>{badgeValue(date)}</InfoBadge>}
+    </View>
+  );
 };
 
 export default Badge;
